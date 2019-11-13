90a2400fdd5f0727b657458ec0d0da30dbc48066
Jules-brendo <julesbreno@hotmail.com>
2019-11-13 13:43:43 -0300

    Add files via upload

---
+++ namespaces/Cliente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace Array
+namespace Banco.Contas
+{
+   public class Cliente
+    {
+        private string p;
+        public Cliente(string p)
+        {
+            this.Nome = p;
+        }
+        public string Nome { get; set; }
+    }
+}
---
+++ namespaces/Contas/ContaInvestimento.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    class ContaInvestimento : Conta, ITributavel
+    {
+        public override void Deposita(double valorOperacao)
+        {
+            this.Saldo += valorOperacao;
+        }
+        public override void Saca(double valor)
+        {
+            this.Saldo -= (valor + 0.10);
+        }
+        public double CalculaTributo()
+        {
+            return this.Saldo * 0.03;
+        }
+    }
+}
